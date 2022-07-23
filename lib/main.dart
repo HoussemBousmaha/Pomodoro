4c80145788c006bb605891f8f858b197fe8d8dc1
Houssem Bousmaha <ky_bousmaha@esi.dz>
2022-07-23 16:48:50 +0100

    Clock And Start Button and number of sessions done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'dart:io';
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Flutter Demo');
+    setWindowMinSize(const Size(400, 450));
+    setWindowMaxSize(Size.infinite);
+  }
   runApp(const Tomatoro());
 }
 
@@ -9,9 +19,12 @@ class Tomatoro extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.openSansTextTheme(),
+      ),
     );
   }
 }
@@ -21,6 +34,152 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFFA65D5E),
+            Color(0xFFA65D5E),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 300,
+                width: 300,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                alignment: Alignment.center,
+                child: CustomPaint(
+                  size: const Size(300, 300),
+                  painter: MyPainter(),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          '25',
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.grey.shade700,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const Text('min.'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 170,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF55A11F),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                        ),
+                      ],
+                    ),
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(Icons.fast_forward, color: Colors.white),
+                        SizedBox(width: 10),
+                        Text(
+                          'Start',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    height: 40,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(Icons.flag, color: Colors.white),
+                        SizedBox(width: 10),
+                        Text(
+                          '0',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.translate(size.width / 2, size.width / 2);
+    const textStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 15,
+    );
+    for (int i = 0; i < 12; i++) {
+      final textSpan = TextSpan(
+        text: '${i * 5}',
+        style: textStyle,
+      );
+      final textPainter = TextPainter(text: textSpan, textDirection: TextDirection.ltr);
+      textPainter.layout(minWidth: 0, maxWidth: size.width);
+
+      final x = ((size.width - 40) / 2) * math.sin(i * 30 * (math.pi / 180)) - textPainter.width / 2;
+      final y = ((size.width - 40) / 2) * -math.cos(i * 30 * (math.pi / 180)) - textPainter.height / 2;
+
+      final offset = Offset(x, y);
+      textPainter.paint(canvas, offset);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
