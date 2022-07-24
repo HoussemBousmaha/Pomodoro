bc1c447b7cb8a71c5957bacf425ba8f647841ac7
Houssem Bousmaha <ky_bousmaha@esi.dz>
2022-07-24 21:53:16 +0100

    App's bg

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:tomatoro/screens/home_screen/clock.dart';
 import 'package:tomatoro/screens/home_screen/start_session_button.dart';
@@ -7,58 +9,53 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            const Color(0xFFA65D5E),
-            Colors.pink.shade300,
-            const Color(0xFFA65D5E),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFF9A3CA).withOpacity(0.6),
         ),
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(height: 30),
-              const Clock(),
-              const SizedBox(height: 30),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const StartSessionButton(),
-                  const SizedBox(width: 10),
-                  Container(
-                    height: 40,
-                    width: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    alignment: Alignment.center,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: const [
-                        Icon(Icons.flag, color: Colors.white),
-                        SizedBox(width: 10),
-                        Text(
-                          '0',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w900,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 30),
+                const Clock(),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const StartSessionButton(),
+                    const SizedBox(width: 10),
+                    Container(
+                      height: 40,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Icon(Icons.flag, color: Colors.white),
+                          SizedBox(width: 10),
+                          Text(
+                            '0',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w900,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
